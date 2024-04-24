6ad970208705f2ffe271632513585e67fb991e6f
nereaferrara02 <nerea.ferrara01@estudiant.upf.edu>
2024-04-24 19:09:26 +0200

    me falta el world render

--- src/framework/world.cpp
+++ src/framework/world.cpp
@@ -13,6 +13,23 @@ World::World()
 	camera->setPerspective(70.f, window_width / (float)window_height, 0.1f, 10000.f); //set the projection, we want to be perspective
 
 
+	Mesh* spitfire_mesh = Mesh::Get("data/meshes");
+	Material spitfire_material;
+	//mat.diffuse = Texture::Get("ok");
+
+	/*
+
+
+	int iterations = 25;
+
+	for (int x = -iterations; x < iterations; ++x) {
+		for (int y = -iterations; y < iterations; ++y) {
+			EntityMesh* spitfire = new EntityMesh(spitfire_mesh, spitfire_material);
+			spitfire->model.setTranslation(x*5,y*5,0);
+			root.addChild(spitfire);
+		}
+	}*/
+
 
 
 }
@@ -21,6 +38,38 @@ World::World()
 void World::render() {
 
 	camera->enable();
+
+
+
+
+	// Render all scene tree
+	root.render(camera);
+
+
+}
+
+
+void World::update(float seconds_elapsed) {
+
+
+
+	if (free_camera) //de les fotos 
+	{
+		float speed = seconds_elapsed * camera_speed;
+
+		camera_pitch = clamp(camera_yaw);
+
+		root.update(seconds_elapsed);
+
+		//player update etc
+	}
+
+
+	for (auto e : entities_to_destroy) {
+		root.removeChild(e);
+		delete e;
+	}
+	entities_to_destroy.clear();
 
 }
 
