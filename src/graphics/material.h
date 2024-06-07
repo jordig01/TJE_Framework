2e6b36ec794ee16d133cbc0d63b9a2f58720f4c2
nereaferrara02 <nerea.ferrara01@estudiant.upf.edu>
2024-06-07 10:57:18 +0200

    Update material.h

--- src/graphics/material.h
+++ src/graphics/material.h
@@ -9,15 +9,13 @@ class Texture;
 class Material {
 public:
 	Shader* shader = nullptr;
+	
 	Vector4 color = Vector4(1.f);
+	
 	Texture* diffuse = nullptr;
-
-
-
-	//-- AGGIUNYTO 28/05 --
 	Texture* normal = nullptr;
 
-	bool transparent =false;
+	bool transparent = false;
 
 	int tiling = 0;
 };
