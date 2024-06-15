65f4628010db590c5baccc0786891aab55727d13
jordig01 <jordi.guillen01@estudiant.upf.edu>
2024-06-15 13:01:56 +0200

    v1

--- src/framework/entities/entityPlayer.cpp
+++ src/framework/entities/entityPlayer.cpp
@@ -387,7 +387,7 @@ void EntityPlayer::handleCollisions(float seconds_elapsed) {
 	}
 
 	if (!is_grounded) {
-		velocity.y -= 40.0f * seconds_elapsed;
+		velocity.y -= 5.0f/* * seconds_elapsed*/;
 	}
 	else if (Input::wasKeyPressed(SDL_SCANCODE_SPACE)) {
 		velocity.y = 2.0f;
